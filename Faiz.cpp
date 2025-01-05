fac6fdaaf433d36cf1f45f3e97135f53070e9a29
ahmadfaizzuddin <ahmadfaizzuddin@student.usm.my>
2025-01-05 23:44:48 +0800

    work done

--- Faiz.cpp
+++ Faiz.cpp
@@ -3,46 +3,164 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
-
+/*
 int main()
 {
     string username;
-    int password, truepassword = 4231, Options ;
-    double Gadget[6][2],Accesories[9][2], Component[12][2];
-
-
-        cout << "Please insert username and password";
+    int password, truepassword = 4231 ;
+    
+    do
+    {
+        cout << "Please insert username:\n";
         getline(cin,username);
+        cout << "Please insert password:\n";
         cin >> password;
 
-        do
-        {
-            cout << " Your password is incorrect";
-            return 1;
-        }while(password != truepassword);
         
-    cout <<"Welcome " << username << endl;
-            return 0;
-}
-
-
-/*else
+        if(password != truepassword)
         {
-            // the update stock function
-           
-            cout << "Please choose the categories that needed update stock:\n";
-            cout << "1. Gadget\n";
-            cout << "2. Accesories\n";
-            cout << "3. Components\n";
-            cin >> Options;
-            if (Options == 1)
+            cout << " Your password is incorrect\n";
+        } 
+    }
+    while(password != truepassword);
+    
+    cout <<"Welcome " << username << endl;
+    return 0;
+}
+*/
+
+int main()                                     //stock level function
+{
+    int Options,row,collum = 2, stock_update;
+    double Gadget[6][2],Accesories[9][2], Component[12][2];
+    for(;;)
+    {       
+    cout << "Please choose the categories that needed update stock:\n";
+    cout << "1. Gadget\n";
+    cout << "2. Accesories\n";
+    cout << "3. Components\n";
+    cin >> Options;
+
+
+    switch (Options)
+    {
+    case 1: 
+    cout << "Please select the item:\n";
+    cout << "1.Samsung A12\n";
+    cout << "2. Samsung Panasonic tv\n";
+    cout << "3. Ps5 \n";
+    cout << "4,Smart watch\n";
+    cout << "5.Laptop\n";
+    cout << "6.Iphone 17 Pro";
+    cout << "6.\n";
+
+    cin >> row;
+
+    cout <<"Please insert the stock:";
+    cin >> stock_update;
+
+    Gadget[row-1][1] = stock_update;
+    for(int i =0 ;i<6 ;i++)
+    {
+        for(int j =0 ; j< 2; j++) // remove the garbage value
+        {
+            if (Gadget[i][j] < 1)
             {
-                
+                Gadget[i][j]=0;
             }
+        }
+    }
+
+    for(int i=0 ; i< 6; i++)   //display all stock 
+    {
+        cout <<"" << Gadget[i][1]<< endl;
+    }
+        break;
+    case 2:
+    cout << "Please select the item:\n";
+    cout << "1.Phone case\n";
+    cout << "2.Charging cable type C\n";
+    cout << "3.Earbud \n";
+    cout << "4.Power bank\n";
+    cout << "5.Keyboard and mouse\n";
+    cout << "6.Mouse Pad\n";
+    cout << "7.Sony speaker\n";
+    cout << "8.Wired earphone\n";
+    cout << "9.ewwwww\n";
+
+
+    cin >> row;
+
+    cout <<"Please insert the stock:";
+    cin >> stock_update;
+
+    Accesories[row-1][1] = stock_update;
+
+    for(int i =0 ;i<6 ;i++) // remove the garbage value
+    {
+        for(int j =0 ; j< 2; j++)
+        {
+            if (Accesories[i][j] < 1)
+            {
+                Accesories[i][j]=0;
+            }
+        }
+    }
+
+    for(int i=0 ; i< 6; i++)   //display all stock 
+    {
+        cout <<"" << Accesories[i][1]<< endl;
+    }
+        break;
+    case 3:
+     cout << "Please select the item:\n";
+    cout << "1.Resistor (5 ohm)\n";
+    cout << "2.Capacitor(1 f)\n";
+    cout << "3.Battery lithium \n";
+    cout << "4.Led light\n";
+    cout << "5.Buzzer\n";
+    cout << "6.Transistor\n";
+    cout << "7.Switch\n";
+    cout << "8.Relay\n";
+    cout << "9.Connecting wire\n";
+    cout << "10.PCB board\n";
+    cout << "11.Bread board\n";
+    cout << "12.Soldering equipment\n";
+
+
+    cin >> row;
+
+    cout <<"Please insert the stock:";
+    cin >> stock_update;
+
+    Component[row-1][1] = stock_update;
+
+    for(int i =0 ;i<6 ;i++)    // remove the garbage value
+    {
+        for(int j =0 ; j< 2; j++)
+        {
+            if (Component[i][j] < 1)
+            {
+                Component[i][j]=0;
+            }
+        }
+    }
+
+    for(int i=0 ; i< 6; i++) //display all stock 
+    {
+        cout <<"" << Component[i][1]<< endl;
+    }
+        break;
+
+    default:
+    cout << "Invalid choice\n";  
+        break;
+    }
+    }
             
 
 
-        }
-*/
+}
