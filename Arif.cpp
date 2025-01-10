2c388c6873fe6aff83b69e42ff70e952c923da90
ahmadfaizzuddin <ahmadfaizzuddin@student.usm.my>
2025-01-11 00:53:57 +0800

    nice one ariff

--- Arif.cpp
+++ Arif.cpp
@@ -6,17 +6,23 @@
 #include <iomanip>
 using namespace std;
 
+int selection; 
+double Gadget[6][2], Accesories[9][2],  Component[12][2];
+string gadget[6][2], accesories[9][2],  component[12][2];
 void displayStock(int selection, double Gadget[6][2], double Accesories[9][2], double Component[12][2], string gadget[6][2], string accesories[9][2], string component[12][2]);
 
+
 int main()
-{
+{   
+    int loopingcondition;
+    do{
     int selection;
     string gadget[6][2] = {{"Samsung A12"}, {"Samsung Panasonic TV"}, {"Ps5"}, {"Smartwatch"}, {"Laptop"}, {"Iphone 17 Pro"}};
     string accesories[9][2] = {{"Phone Case"}, {"Charging Cable Type"}, {"Earbud"}, {"Powerbank"}, {"Keyboard & Mouse"}, {"Mousepad"}, {"Sony Speaker"}, {"Wired Headphone"}};  
     string component[12][2] = {{"Resistor"},{"Capacitor"},{"Lithium Battery"},{"LED Light"},{"Buzzer"},{"Transistor"},{"Switch"},{"Relay"},{"Connecting Wire"},{"PCB Board"},{"Breadboard"},{"Soldering Equipment"}};    
-    double Component[12][2] = {{0,24},{0,33},{0,12},{0,1},{0,5},{0,17},{0,22},{0,56},{0,34},{0,11},{0,7},{0,34}};
-    double Gadget[6][2] = {{0,23},{0,43},{0,23},{0,12},{0,44},{0,10}};
-    double Accesories[9][2] = {{0,34},{0,12},{0,9},{0,21},{0,2},{0,23},{0,15},{0,16},{0,10}};
+    //double Component[12][2] = {{0,24},{0,33},{0,12},{0,1},{0,5},{0,17},{0,22},{0,56},{0,34},{0,11},{0,7},{0,34}};
+    //double Gadget[6][2] = {{0,23},{0,43},{0,23},{0,12},{0,44},{0,10}};
+    //double Accesories[9][2] = {{0,34},{0,12},{0,9},{0,21},{0,2},{0,23},{0,15},{0,16},{0,10}};
 
     cout << "\n[1] Gadgets";
     cout << "\n[2] Accesories";
@@ -27,8 +33,18 @@ int main()
     cout << " " << endl;
 
     displayStock(selection,Gadget,Accesories,Component,gadget,accesories,component);
-    return 0;
+    
+    cout<<"Do you wish to continue?\n";
+    cout<<"1.Yes\n2.No\n";
+    cin>>loopingcondition;
+    }
+    while (loopingcondition==1);
+    {
+        return 0;
+    }
+    
 }
+
 void displayStock(int selection, double Gadget[6][2], double Accesories[9][2], double Component[12][2], string gadget[6][2], string accesories[9][2], string component[12][2])
 {
     int i,j;
