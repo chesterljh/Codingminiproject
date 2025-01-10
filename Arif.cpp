7fc22954d37729f18129393d11712b33df1f5f21
ahmadfaizzuddin <ahmadfaizzuddin@student.usm.my>
2025-01-11 01:11:20 +0800

    nice job ariff

--- Arif.cpp
+++ Arif.cpp
@@ -7,7 +7,9 @@
 using namespace std;
 
 int selection; 
-double Gadget[6][2], Accesories[9][2],  Component[12][2];
+double Component[12][2] = {{0,24},{0,33},{0,12},{0,1},{0,5},{0,17},{0,22},{0,56},{0,34},{0,11},{0,7},{0,34}};
+double Gadget[6][2] = {{0,23},{0,43},{0,23},{0,12},{0,44},{0,10}};
+double Accesories[9][2] = {{0,34},{0,12},{0,9},{0,21},{0,2},{0,23},{0,15},{0,16},{0,10}};
 string gadget[6][2], accesories[9][2],  component[12][2];
 void displayStock(int selection, double Gadget[6][2], double Accesories[9][2], double Component[12][2], string gadget[6][2], string accesories[9][2], string component[12][2]);
 
@@ -20,9 +22,7 @@ int main()
     string gadget[6][2] = {{"Samsung A12"}, {"Samsung Panasonic TV"}, {"Ps5"}, {"Smartwatch"}, {"Laptop"}, {"Iphone 17 Pro"}};
     string accesories[9][2] = {{"Phone Case"}, {"Charging Cable Type"}, {"Earbud"}, {"Powerbank"}, {"Keyboard & Mouse"}, {"Mousepad"}, {"Sony Speaker"}, {"Wired Headphone"}};  
     string component[12][2] = {{"Resistor"},{"Capacitor"},{"Lithium Battery"},{"LED Light"},{"Buzzer"},{"Transistor"},{"Switch"},{"Relay"},{"Connecting Wire"},{"PCB Board"},{"Breadboard"},{"Soldering Equipment"}};    
-    //double Component[12][2] = {{0,24},{0,33},{0,12},{0,1},{0,5},{0,17},{0,22},{0,56},{0,34},{0,11},{0,7},{0,34}};
-    //double Gadget[6][2] = {{0,23},{0,43},{0,23},{0,12},{0,44},{0,10}};
-    //double Accesories[9][2] = {{0,34},{0,12},{0,9},{0,21},{0,2},{0,23},{0,15},{0,16},{0,10}};
+
 
     cout << "\n[1] Gadgets";
     cout << "\n[2] Accesories";
